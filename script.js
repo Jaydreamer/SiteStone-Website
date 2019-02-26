ff54925e2012bbf839acf1fb5af66da6f6b3db9b
Jarrod Bowles <47378716+Jaydreamer@users.noreply.github.com>
2019-02-26 14:22:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-
+//scroll to top function
+window.onload = function() {
+ jQuery(function($) {
+   var offset = {{ vertical_offset_for_trigger }};
+   var duration = 500;
+   $(window).scroll(function() {
+     if ($(this).scrollTop() > offset) {
+       $('.back-to-top').fadeIn(duration);
+     } 
+     else {
+       $('.back-to-top').fadeOut(duration);
+     }
+   });
+   $('.back-to-top').unbind('click.smoothscroll').bind('click', function(e) {
+     e.preventDefault();
+     $('html, body').animate( { scrollTop: 0 }, duration);
+     return false;
+   })
+ });
+}
