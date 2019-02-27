c91757ac58f727c443168f3ebb586079eacb0fb3
Jaydreamer <jayways45@gmail.com>
2019-02-27 07:29:39 +0200

    updates

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.onload = function() {
    $(window).scroll(function() {
      if ($(this).scrollTop() > offset) {
        $('.back-to-top').fadeIn(duration);
-     } 
+     }
      else {
        $('.back-to-top').fadeOut(duration);
      }
@@ -17,4 +17,35 @@ window.onload = function() {
      return false;
    })
  });
+}
+$(function scroll_up(){
+      $("#includedContent").load("scroll_to_top.html");
+    });
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
 }
